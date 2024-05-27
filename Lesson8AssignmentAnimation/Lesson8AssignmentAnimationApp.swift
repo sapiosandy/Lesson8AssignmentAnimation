291ae5051e3986bef5a5f3d81c4b67d4434d93a1
Sandy Gomez <sgomez1029@gmail.com>
2024-05-27 15:46:19 -0700

    Initial Commit

---
+++ Lesson8AssignmentAnimation/Lesson8AssignmentAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson8AssignmentAnimationApp.swift
+//  Lesson8AssignmentAnimation
+//
+//  Created by Sandra Gomez on 5/27/24.
+//
+
+import SwiftUI
+
+@main
+struct Lesson8AssignmentAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
