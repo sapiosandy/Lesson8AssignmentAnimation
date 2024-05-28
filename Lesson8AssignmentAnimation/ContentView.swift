49635de5f32a20a89614c17668dd10a68edb087b
Sandy Gomez <sgomez1029@gmail.com>
2024-05-27 20:41:09 -0700

    Lesson 8 Assignment Animation

--- Lesson8AssignmentAnimation/ContentView.swift
+++ Lesson8AssignmentAnimation/ContentView.swift
@@ -4,21 +4,63 @@
 //
 //  Created by Sandra Gomez on 5/27/24.
 //
-
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isMoved = false
+    @State private var scaleBlue: CGFloat = 1.0
+    @State private var scaleYellow: CGFloat = 1.0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 20) {
+            Button(action: {
+                withAnimation(.easeIn(duration: 1.0)) {
+                    self.scaleBlue = self.scaleBlue == 1.0 ? 1.5 : 1.0
+                }
+            }) {
+                Text("Tap me")
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .clipShape(Capsule())
+                    .scaleEffect(scaleBlue)
+            }
+            
+            Rectangle()
+                .fill(Color.red)
+                .frame(width: 100, height: 100)
+                .offset(x: isMoved ? 150 : 0)
+                .animation(.easeOut(duration: 1.0), value: isMoved)
+            
+            Rectangle()
+                .fill(Color.green)
+                .frame(width: 100, height: 100)
+                .offset(x: isMoved ? -150 : 0)
+                .animation(.easeInOut(duration: 1.0), value: isMoved)
+            
+            Button(action: {
+                withAnimation(.spring(response: 0.5, dampingFraction: 0.3, blendDuration: 0)) {
+                    self.scaleYellow = self.scaleYellow == 1.0 ? 1.5 : 1.0
+                }
+            }) {
+                Text("Tap me")
+                    .padding()
+                    .background(Color.yellow)
+                    .foregroundColor(.white)
+                    .clipShape(Capsule())
+                    .scaleEffect(scaleYellow)
+            }
         }
-        .padding()
+        .onTapGesture {
+            isMoved.toggle()
+        }
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
+
